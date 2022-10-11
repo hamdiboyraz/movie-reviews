fdb793ce767945c66958f8131fb95ce4af77757c
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-10-12 01:26:37 +0300

    Add a search

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,10 @@ from .models import Movie
 # Create your views here.
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all() 
+    if searchTerm:
+        movies= Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all() 
     return render(request, 'home.html', {'search_Term':searchTerm, 'movies':movies})
 
 def about(request):
