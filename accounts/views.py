85a77396dab33d0b3679cd37e886059e1ca3ffad
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-10-15 22:56:10 +0300

    Implement passwords do not match error

--- accounts/views.py
+++ accounts/views.py
@@ -16,4 +16,6 @@ def signupaccount(request):
             user.save()
             login(request, user)
             return redirect('home')
+        else:
+            return render(request, 'signupaccount.html', {'form':UserCreationForm, 'error':'Passwords do not match'})
 
