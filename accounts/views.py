95122e0e9e84ac848b0134beeeecf47784c48612
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-10-17 00:31:43 +0300

    Implement logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.shortcuts import redirect
 from django.db import IntegrityError
 
@@ -22,3 +22,7 @@ def signupaccount(request):
                 return render(request, 'signupaccount.html', {'form':UserCreateForm, 'error':'Username already taken. Choose new username.'})
         else:
             return render(request, 'signupaccount.html', {'form':UserCreateForm, 'error':'Passwords do not match'})
+
+def logoutaccount(request):
+    logout(request)
+    return redirect('home')
