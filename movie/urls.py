71d30d62b6b372459617e85cd8be1bed6efcb8bc
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-10-17 17:13:20 +0300

    Add url and function for update review

--- movie/urls.py
+++ movie/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:movie_id>', views.detail, name='detail'),
     path('<int:movie_id>/create', views.createReview, name='createreview'),
+    path('review/<int:review_id>', views.updateReview, name='updatereview'), 
 ]
+
+# updatereview -> http://localhost:8000/movie/review/2
